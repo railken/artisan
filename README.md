54a931d1471bf6f6f4ccce62fbeb130c254b2a14
railken <railken@protonmail.ch>
2019-06-21 15:01:16 +0200

    working artisan

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-artisan
+# artisan
+
+[![Build Status](https://travis-ci.org/railken/artisan.svg?branch=master)](https://travis-ci.org/railken/artisan)
+
+# Requirements
+
+PHP 7.1 and later.
+
+## Installation
+
+You can install it via [Composer](https://getcomposer.org/) by typing the following command:
+
+```bash
+composer require railken/artisan
+```
---
+++ src/ApplicationFinder.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Railken\Artisan;
+
+use ReflectionClass;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use Orchestra\Testbench\TestCase;
+
+class ApplicationFinder
+{
+    /**
+     * get the class namespace form file path using token
+     *
+     * @param $filePathName
+     *
+     * @return  null|string
+     */
+    protected function getClassNamespaceFromFile($filePathName)
+    {
+        $src = file_get_contents($filePathName);
+
+        $tokens = token_get_all($src);
+        $count = count($tokens);
+        $i = 0;
+        $namespace = '';
+        $namespace_ok = false;
+        while ($i < $count) {
+            $token = $tokens[$i];
+            if (is_array($token) && $token[0] === T_NAMESPACE) {
+                // Found namespace declaration
+                while (++$i < $count) {
+                    if ($tokens[$i] === ';') {
+                        $namespace_ok = true;
+                        $namespace = trim($namespace);
+                        break;
+                    }
+                    $namespace .= is_array($tokens[$i]) ? $tokens[$i][1] : $tokens[$i];
+                }
+                break;
+            }
+            $i++;
+        }
+        if (!$namespace_ok) {
+            return null;
+        } else {
+            return $namespace;
+        }
+    }
+
+    /**
+     * get the class name form file path using token
+     *
+     * @param $filePathName
+     *
+     * @return  mixed
+     */
+    protected function getClassNameFromFile($filePathName)
+    {
+        $php_code = file_get_contents($filePathName);
+
+        $classes = array();
+        $tokens = token_get_all($php_code);
+        $count = count($tokens);
+        for ($i = 2; $i < $count; $i++) {
+            if ($tokens[$i - 2][0] == T_CLASS
+                && $tokens[$i - 1][0] == T_WHITESPACE
+                && $tokens[$i][0] == T_STRING
+            ) {
+
+                $class_name = $tokens[$i][1];
+                $classes[] = $class_name;
+            }
+        }
+
+        return count($classes) > 0 ? $classes[0] : null;
+    }
+
+    public function findTestFiles($path)
+    {
+        $rii = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
+
+        $files = array(); 
+
+        foreach ($rii as $file) {
+
+            if ($file->isDir()){ 
+                continue;
+            }
+
+            $files[] = $file->getPathname(); 
+
+        }
+
+        return $files;
+    }
+
+    public function findApplication()
+    {
+
+        $classes = [];
+
+        foreach ($this->findTestFiles(getcwd()."/tests") as $file) {
+            if (is_file($file)) {
+                $class = $this->getClassNamespaceFromFile($file) . "\\" . $this->getClassNameFromFile($file);
+
+                if (is_subclass_of($class, TestCase::class)) {
+
+                    $reflection = new ReflectionClass($class);
+
+                    if (!$reflection->isAbstract()) {
+                        $classes[] = $class;
+                    }
+                }
+            }
+        }
+
+        $test = new $classes[0];
+        $test->setUp();
+
+        $reflection = new ReflectionClass($test);
+
+        $property = $reflection->getProperty('app');
+        $property->setAccessible(true);
+        $app = $property->getValue($test);
+
+        return $app;
+    }
+
+}
